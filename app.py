45dd4e88a88cd7a021488639c523cc4030d99395
Vyom <150064126+svyoma@users.noreply.github.com>
2025-04-16 21:45:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,6 +101,5 @@ def index():
     return render_template_string(HTML_TEMPLATE, forms=forms)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-    dev_text = transliterate.process('IAST', 'Devanagari', 'kattikassa')  # example word
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
